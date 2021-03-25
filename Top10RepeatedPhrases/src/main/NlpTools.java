8f05b0d6a719520caebf87429afb55c1e4c1ea95
Dan Williams <williams.dan@husky.neu.edu>
2021-03-25 01:05:22 -0700

    Additional changes to project structure

--- Top10RepeatedPhrases/src/main/NlpTools.java
+++ Top10RepeatedPhrases/src/main/NlpTools.java
@@ -106,7 +106,3 @@ public class NlpTools {
     return topPhrases;
   }
 }
-
-// The quick brown fox jumped over the lazy dog. The lazy dog, peeved to be labeled lazy, jumped over a snoring turtle. In retaliation the quick brown fox jumped over ten snoring turtles. Then the quick brown fox refueled with some ice cream.
-
-// Cool calm and collected. Cool and collected. Cool calm and collected.
